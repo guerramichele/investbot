754f5f9702c78d038976097f23eecf24a3cbac36
guerramichele <guerra.michele.bsnss@gmail.com>
2025-12-17 19:20:15 +0100

    Refactor server.js for cleaner route handling

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
 app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'build', 'index.html')));
 const PORT = process.env.PORT || 10000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log('Server OK'));

8f7b2934f7e32abdc0018e7838207c8deb905c57
guerramichele <guerra.michele.bsnss@gmail.com>
2025-12-17 18:57:49 +0100

    Initialize Express server to serve static files

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle React routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
